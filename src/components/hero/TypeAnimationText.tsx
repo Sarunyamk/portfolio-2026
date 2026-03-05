9000b25d8dae851660ab8fae9080d62836967e2b
Sarunya <sarunya46mk@gmail.com>
2026-03-05 12:09:11 +0700

    feat : add animation type text

---
+++ src/components/hero/TypeAnimationText.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { TypeAnimation } from 'react-type-animation';
+
+export default function TypeAnimationText() {
+  return (
+    <TypeAnimation
+      sequence={[
+        'Full Stack Developer',
+        1500,
+        'Frontend Developer',
+        1500,
+        'Backend Developer',
+        1500,
+        'Web Developer',
+        1500,
+        'UI/UX Designer',
+        1500,
+      ]}
+      speed={50}
+      omitDeletionAnimation={true}
+      repeat={Infinity}
+      className="md:font-head text-muted-foreground mb-6"
+    />
+  );
+}

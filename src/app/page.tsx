4a086993d8ac7ce94fa5ef00df216da9811fb2c8
Sarunya <sarunya46mk@gmail.com>
2026-03-05 12:08:19 +0700

    chore : init project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from '@/components/hero/Hero';
+import Navbar from '@/components/navigate/Navbar';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}

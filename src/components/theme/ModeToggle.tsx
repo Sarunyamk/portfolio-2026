ede8ef55531d0905fd071e5275102e7b9f6e4d8d
Sarunya <sarunya46mk@gmail.com>
2026-04-06 11:47:52 +0700

    fix(mobile): edit text-color modeToggle and LangToggle

--- src/components/theme/ModeToggle.tsx
+++ src/components/theme/ModeToggle.tsx
@@ -19,8 +19,8 @@ export function ModeToggle() {
       onClick={toggleTheme}
       className="relative"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun className="h-[1.2rem] w-[1.2rem] transition-all dark:scale-0 dark:-rotate-90 text-primary" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 text-primary" />
     </Button>
   );
 }
